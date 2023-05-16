94ba0454b70a1ca0a79a64c63b8077a7c8e2eba4
RR.Nigmatullin <radelnigma@yandex.ru>
2023-05-16 20:22:53 +0300

    ver2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,55 @@
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void printResult(long sec) {
+        int hour = (int) (Math.abs(sec) / 3600);
+        int minute = (int) ((Math.abs(sec)  % 3600) / 60);
+        int second = (int) (Math.abs(sec)  % 60);
+        String formatTime =
+                second != 0
+                        ? String.format("%02d:%02d:%02d", hour, minute, second)
+                        : String.format("%02d:%02d", hour, minute);
 
+        if (sec >= 0) {
+            System.out.println("\tРезультат: " + formatTime);
+        } else {
+            System.out.println("\tРезультат: -" + formatTime);
+        }
+    }
+
+    public static String getFormattedLine (String line) {
+        char[] chars = line.toCharArray();
+        StringBuilder token = new StringBuilder();
+
+        for (char ch : chars) {
+            if (Pattern.matches("[\\d.,:;]", String.valueOf(ch))) {
+                if (Pattern.matches("[.,:;]", String.valueOf(ch))){
+                    ch = '.';
+                }
+                token.append(ch);
+            } else if (Pattern.matches("[+\\-*/]", String.valueOf(ch))){
+                token.append(" ").append(ch).append(" ");
+            }
+        }
+        return String.valueOf(token);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Введите выражение например:\n" +
+                "1.25+3.45-5.55*2/3\n" +
+                "Где целая часть - часы, часть после запятой - минуты\n" +
+                "В качестве разделителя допускается использовать '.' ',' ':' ';'\n" +
+                "Так же результат можно умножать и делить на целое число\n");
         String token = "";
         while (!token.equals("exit")) {
-            System.out.println("Введи выражение в вид");
+            System.out.print("Введите выражение:\t");
             Scanner scanner = new Scanner(System.in);
             String line = scanner.nextLine();
-            List<String> list = Arrays.stream(line.split(" ")).toList();
-            LocalTime localTime = null;
+            String[] list = getFormattedLine(line).split(" ");
+            LocalTime localTime;
             String operator = "";
             long sec = 0;
             try {
@@ -39,7 +74,7 @@ public class Main {
                         operator = token;
                     }
                 }
-                System.out.println("Результат: " + LocalTime.ofSecondOfDay(sec));
+                printResult(sec);
             } catch (RuntimeException e) {
                 System.out.println(e.getMessage());
             }
