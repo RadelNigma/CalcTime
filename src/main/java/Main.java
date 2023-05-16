c3964d83828366c27c8ef2ffa5c1b7c777642c4d
RadelNigma <radelnigma@yandex.ru>
2023-05-17 01:42:46 +0300

    ver3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void printResult(long sec) {
         int hour = (int) (Math.abs(sec) / 3600);
-        int minute = (int) ((Math.abs(sec)  % 3600) / 60);
-        int second = (int) (Math.abs(sec)  % 60);
+        int minute = (int) ((Math.abs(sec) % 3600) / 60);
+        int second = (int) (Math.abs(sec) % 60);
         String formatTime =
                 second != 0
                         ? String.format("%02d:%02d:%02d", hour, minute, second)
@@ -15,26 +13,69 @@ public class Main {
 
         if (sec >= 0) {
             System.out.println("\tРезультат: " + formatTime);
-        } else {
+        } else if (sec != -1){
             System.out.println("\tРезультат: -" + formatTime);
         }
     }
 
-    public static String getFormattedLine (String line) {
+    public static String getFormattedLine(String line) {
         char[] chars = line.toCharArray();
         StringBuilder token = new StringBuilder();
 
         for (char ch : chars) {
             if (Pattern.matches("[\\d.,:;]", String.valueOf(ch))) {
-                if (Pattern.matches("[.,:;]", String.valueOf(ch))){
+                if (Pattern.matches("[.,:;]", String.valueOf(ch))) {
                     ch = '.';
                 }
                 token.append(ch);
-            } else if (Pattern.matches("[+\\-*/]", String.valueOf(ch))){
+            } else if (Pattern.matches("[+\\-*/]", String.valueOf(ch))) {
                 token.append(" ").append(ch).append(" ");
+            } else {
+                System.out.println("Недопустимый символ " + ch);
+                return "";
             }
         }
         return String.valueOf(token);
+    }
+
+    public static long getSeconds(String[] list) {
+        long sec = 0;
+        String operator = "";
+        try {
+            for (String token : list) {
+                if (!token.matches("\\d+\\.\\d+") && !token.matches("[+\\-*/]")) {
+                    System.out.printf("Некорректный ввод %s\n", token);
+                    return -1;
+                }
+                if (token.matches("\\d+\\.\\d+")) {
+                    long hour = Long.parseLong(token.split("\\.")[0]);
+                    long minute = Long.parseLong(token.split("\\.")[1]);
+                    if (minute > 59) {
+                        System.out.printf("Некорректный ввод %02d минут! Корректный ввод [0-59]\n", minute);
+                        return -1;
+                    }
+                    long tokenSeconds = hour * 3600 + minute * 60;
+                    if (operator.contains("+")) {
+                        sec += tokenSeconds;
+                    } else if (operator.contains("-")) {
+                        sec -= tokenSeconds;
+                    } else {
+                        sec = tokenSeconds;
+                    }
+                } else if (token.matches("\\d")) {
+                    if (operator.contains("*")) {
+                        sec *= Long.parseLong(token);
+                    } else if (operator.contains("/")) {
+                        sec /= Long.parseLong(token);
+                    }
+                } else {
+                    operator = token;
+                }
+            }
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        return sec;
     }
 
     public static void main(String[] args) {
@@ -43,41 +84,12 @@ public class Main {
                 "Где целая часть - часы, часть после запятой - минуты\n" +
                 "В качестве разделителя допускается использовать '.' ',' ':' ';'\n" +
                 "Так же результат можно умножать и делить на целое число\n");
-        String token = "";
-        while (!token.equals("exit")) {
+        while (true) {
             System.out.print("Введите выражение:\t");
             Scanner scanner = new Scanner(System.in);
             String line = scanner.nextLine();
             String[] list = getFormattedLine(line).split(" ");
-            LocalTime localTime;
-            String operator = "";
-            long sec = 0;
-            try {
-                for (String s : list) {
-                    token = s;
-                    if (token.matches("\\d+\\.\\d{2}")) {
-                        localTime = LocalTime.parse(token, DateTimeFormatter.ofPattern("H.m"));
-                        if (operator.contains("+")) {
-                            sec += localTime.toSecondOfDay();
-                        } else if (operator.contains("-")) {
-                            sec -= localTime.toSecondOfDay();
-                        } else {
-                            sec = localTime.toSecondOfDay();
-                        }
-                    } else if (token.matches("\\d")) {
-                        if (operator.contains("*")) {
-                            sec *= Long.parseLong(token);
-                        } else if (operator.contains("/")) {
-                            sec /= Long.parseLong(token);
-                        }
-                    } else {
-                        operator = token;
-                    }
-                }
-                printResult(sec);
-            } catch (RuntimeException e) {
-                System.out.println(e.getMessage());
-            }
+            printResult(getSeconds(list));
         }
     }
 }
